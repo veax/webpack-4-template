0ed79ba56b87ab7db430cdbce40fcc948bbbc1cb
veax <borissowadim@mail.ru>
2018-07-28 00:41:30 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.sass");
+require("./images/link.jpg");
+require("./index.html");
+
+// const bar = () => "baz";
+// console.log(foo);
---
+++ webpack.dev.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: ["./src/main.js"]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true,
+    hot: true,
+    stats: {
+      colors: true
+    }
+  },
+  devtool: "cheap-module-eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            // translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
